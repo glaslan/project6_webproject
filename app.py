0a1e5007676c4537ee75d52899ccc883e3a2a683
glaslan <glasd6702@gmail.com>
2026-01-14 16:03:35 -0500

    Add pre-commit and ran

--- app.py
+++ app.py
@@ -2,13 +2,16 @@ from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
 
-@app.route('/health')
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/health")
 def health():
     return jsonify({"status": "healthy"})
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=4000, debug=True)
