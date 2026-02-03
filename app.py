1f70433059612e33ccdd78bb79ab385e30978a5a
glaslan <glasd6702@gmail.com>
2026-02-03 10:50:10 -0500

    Add docstings

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+""" This module is the main entry point for the Flask app """
+
 from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
@@ -5,11 +7,21 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    """ 
+    Default route for the index page 
+    Returns: 
+    template: The index page html template
+    """
     return render_template("index.html")
 
 
 @app.route("/health")
 def health():
+    """ 
+    Default route for checking that the website is up and reachable
+    Returns: 
+    json: A json object indicating whether the website is healthy
+    """
     return jsonify({"status": "healthy"})
 
 
